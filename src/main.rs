d9a2d5d23244aa027f9827f02ba558e58331f2f4
derrick921213 <dlin12457@gmail.com>
2026-05-06 15:08:40 +0800

    Add Material Design components and styles
    - Introduced a new ToolTip component for displaying text with a background.
    - Added a Vertical component for consistent padding and spacing.
    - Included various SVG icons such as arrow, calendar, check, and menu icons.
    - Created data structures for ListItem, MenuItem, and NavigationItem to support UI elements.
    - Implemented MaterialPalette and MaterialTypography for theming and text styles.
    - Defined MaterialAnimations for consistent UI transitions.
    - Removed deprecated test.slint file and added a new main.slint for the main application layout.
    - Updated main.rs to include a simple counter application using Slint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+slint::slint! {
+    import { Button,VerticalBox } from "std-widgets.slint";
+    export component App inherits Window {
+        in property <int> counter: 1;
+        callback clicked <=> btn.clicked;
+        VerticalBox {
+            Text {
+                text: "Hello, World";
+            }
+            btn := Button {
+                text: "yaya";
+            }
+        }
+    }
+}
 fn main() {
+    let app = App::new().unwrap();
+    let weak = app.as_weak();
+    app.on_clicked(move || {
+        if let Some(app) = weak.upgrade() {
+            app.set_counter(app.get_counter() + 2);
+        }
+    });
+    app.run().unwrap();
     println!("Hello, world!");
 }
