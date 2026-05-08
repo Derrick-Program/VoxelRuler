d479833f1dc00e6af45167f4ed5b226a28246250
derrick921213 <dlin12457@gmail.com>
2026-05-08 23:53:26 +0800

    更新 Slint 設定，新增主題和資產路徑；新增多個 SVG 圖示檔案；新增側邊欄和按鈕元件，並定義其功能；新增頁腳元件，顯示版本資訊；更新主應用程式以整合新元件。

--- build.rs
+++ build.rs
@@ -5,10 +5,15 @@ fn main() {
     let pj_path = std::path::Path::new(&pj_path);
     let config = slint_build::CompilerConfiguration::new()
         .with_bundled_translations(pj_path.join("ui/translations"))
-        .with_library_paths(std::collections::HashMap::from([(
-            "material".to_string(),
-            pj_path.join("ui/libs/material-1.0/material.slint"),
-        )]))
+        .with_library_paths(std::collections::HashMap::from([
+            (
+                "material".to_string(),
+                pj_path.join("ui/libs/material-1.0/material.slint"),
+            ),
+            ("theme".to_string(), pj_path.join("ui/theme.slint")),
+            ("assets".to_string(), pj_path.join("ui/assets/index.slint")),
+        ]))
+        .with_include_paths(vec![std::path::PathBuf::from("ui")])
         .embed_resources(EmbedResourcesKind::EmbedFiles);
     slint_build::compile_with_config(pj_path.join("ui/main.slint"), config).unwrap();
 }
