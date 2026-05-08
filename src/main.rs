02a2dcb1aa1980a4f2e2a1ad4b883ba3664076f5
derrick921213 <dlin12457@gmail.com>
2026-05-08 17:03:55 +0800

    更新 Slint 依賴設定，移除 gettext 功能，並在主函數中啟用繁體中文翻譯

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 slint::include_modules!();
 fn main() -> anyhow::Result<()>{
-    // slint::select_bundled_translation("").unwrap();
     // slint::init_translations!(concat!(env!("CARGO_MANIFEST_DIR"), "/ui/translations"));
     let app = MainApp::new()?;
+    slint::select_bundled_translation("zh_TW").unwrap();
     // let app_weak = app.as_weak();
     app.run()?;
     Ok(())
